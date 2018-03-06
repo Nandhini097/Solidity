bf745176df98628fec29b5c28d9beac8f850e235
Nandhini097 <37014219+Nandhini097@users.noreply.github.com>
2018-03-06 15:23:33 +0530

    Create library.sol

---
+++ library.sol
@@ -0,0 +1,62 @@
+pragma solidity^0.4.0;
+contract sam
+{
+    struct lib
+    {
+        uint256 bookid;
+        string bookname;
+        bool stock;
+        uint256 count;
+    
+    }
+    struct student
+    {
+        uint256 stuid;
+        uint256 stubookid;
+        string name;
+    }
+    mapping(uint256=>lib)libb;
+    mapping(uint256=>student)stud;
+    uint256 t;
+    function sam()
+    {
+        t=now+1 hours;
+    }
+    modifier ab()
+    {
+        require(t>now);
+        _;
+    }
+    
+    function lib1(uint256 bookid,string bookname,bool stock,uint256 stuid)public ab() 
+    {
+        libb[stuid].bookid=bookid;
+        libb[stuid].bookname=bookname;
+        libb[stuid].stock=true;
+        libb[stuid].count++;
+    }
+    function student1(uint256 stuid,uint256 stubookid,string name)
+    {
+        stud[stuid].stuid=stuid;
+        stud[stuid].stubookid=stubookid;
+        stud[stuid].name=name;
+    }
+    
+    function avail(uint256 id)public constant returns(string)
+    {
+        require(libb[id].stock);
+        
+            return "available";
+            
+       
+        
+    }
+    function studentdetails(uint256 stuid)public constant returns(string,uint256)
+    {          
+       return (libb[stuid].bookname,libb[stuid].count);
+    
+    }
+    
+}
+
+
